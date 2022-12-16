90e204f13aba479ef8398a41f09c37e716bab381
Orangeking321 <120720351+Orangeking321@users.noreply.github.com>
2022-12-16 18:09:28 +0530

    Adding Player and basic movement

--- My project (12)/Assets/Scripts/MovementPlayer.cs
+++ My project (12)/Assets/Scripts/MovementPlayer.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class MovementPlayer : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float speed;
+    bool FacingRight = true;
 
     // Start is called before the first frame update
     private void Awake()
@@ -16,12 +17,30 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Get the horizontal movement from input system
         float horizontalInput = Input.GetAxis("Horizontal");
+        //Get the vertical movement from input system
         float VerticalInput = Input.GetAxis("Vertical");
 
+        //Move according to player Input
         body.velocity = new Vector2(horizontalInput * speed, VerticalInput * speed);
 
-        //body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-        //body.velocity = new Vector2(body.velocity.y, VerticalInput * speed);
+        //Flipping the player with right/left movement
+        if(horizontalInput<0 && FacingRight)
+        {
+            flip();
+        }
+
+        else if (horizontalInput>0 && !FacingRight)
+        {
+            flip();
+        }
+
+    }
+
+    void flip()
+    {
+        FacingRight = !FacingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
