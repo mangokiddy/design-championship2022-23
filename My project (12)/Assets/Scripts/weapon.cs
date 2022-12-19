13eb697a8c3b65e6e9d885bb9943f2ffc2785e46
mangokiddy <120718300+mangokiddy@users.noreply.github.com>
2022-12-19 18:16:54 +0530

    shooting(part2)

--- My project (12)/Assets/Scripts/weapon.cs
+++ My project (12)/Assets/Scripts/weapon.cs
@@ -6,6 +6,7 @@ public class weapon : MonoBehaviour
 {
     public Transform firePoint;
     public int damage = 40;
+    public int delay = 500;
     public LineRenderer line;
 
     // Update is called once per frame
@@ -15,28 +16,35 @@ public class weapon : MonoBehaviour
         {
             StartCoroutine(Shoot());
         }
+        delay -= 1;
     }
     IEnumerator Shoot()
     {
         RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position,firePoint.right);
-        if(hitInfo)
+        if(delay == 0||delay<=0)
         {
-            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
-            if(enemy != null)
+            if(hitInfo)
             {
-                enemy.TakeDamage(damage);
+                Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+                if(enemy != null)
+                {
+                    enemy.TakeDamage(damage);
+                }
+                line.SetPosition(0, firePoint.position);
+                line.SetPosition(1, hitInfo.point);
             }
-            line.SetPosition(0, firePoint.position);
-            line.SetPosition(1, hitInfo.point);
-        }
-        else
-        {
-            line.SetPosition(0, firePoint.position);
-            line.SetPosition(1, firePoint.position + firePoint.right*100);
+            else
+            {
+                line.SetPosition(0, firePoint.position);
+                line.SetPosition(1, firePoint.position + firePoint.right*100);
+            }
+        
+            line.enabled = true;
+            yield return new WaitForSeconds(0.02f);
+            line.enabled = false;
+            delay = 240;
         }
         
-        line.enabled = true;
-        yield return new WaitForSeconds(0.02f);
-        line.enabled = false;
+        
     }
 }
