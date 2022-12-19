712cf4f2f2b8e2ccd4fc4d14aa450f6ba7a6e994
mangokiddy <120718300+mangokiddy@users.noreply.github.com>
2022-12-19 09:48:32 +0530

    camera follow

---
+++ My project (12)/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 10);
+    }
+}
