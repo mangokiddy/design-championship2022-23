becc22a5b5cfa4f2080759b36aaf9d20882c6431
mangokiddy <120718300+mangokiddy@users.noreply.github.com>
2022-12-19 11:54:32 +0530

    a lot of stuff

---
+++ My project (12)/Assets/Scripts/weapon.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public int damage = 40;
+    public LineRenderer line;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+    IEnumerator Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position,firePoint.right);
+        if(hitInfo)
+        {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            line.SetPosition(0, firePoint.position);
+            line.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            line.SetPosition(0, firePoint.position);
+            line.SetPosition(1, firePoint.position + firePoint.right*100);
+        }
+        
+        line.enabled = true;
+        yield return new WaitForSeconds(0.02f);
+        line.enabled = false;
+    }
+}
