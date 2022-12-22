c02c85af7d40124404ee805fb81e22008096edcd
Orangeking321 <120720351+Orangeking321@users.noreply.github.com>
2022-12-22 22:56:14 +0530

    stuff

---
+++ My project (12)/Assets/Scripts/OpenPaper.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPaper : MonoBehaviour
+{
+    public GameObject ThePaper;
+
+    private void Start()
+    {
+        ThePaper = GameObject.Find("Paper");
+        ThePaper.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "player")
+        {
+           ThePaper.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.name == "player")
+        {
+           ThePaper.SetActive(false);
+        }
+    }
+}
