186f9999ae5ebbda9d272ed07ac0c37b89f3b008
Orangeking321 <120720351+Orangeking321@users.noreply.github.com>
2022-12-30 10:59:16 +0530

    heart

---
+++ My project (12)/Assets/Heart.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "player")
+        {
+            GameObject.Find("player").GetComponent<MovementPlayer>().currenthealth = 5;
+            Destroy(GameObject.Find("heart"), 0.09f);
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
