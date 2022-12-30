20183ddc55d3342d2eed31bd05305af0bb5a11aa
Orangeking321 <120720351+Orangeking321@users.noreply.github.com>
2022-12-30 22:56:24 +0530

    not Much

--- My project (12)/Assets/Prefabs/enable.cs
+++ My project (12)/Assets/Prefabs/enable.cs
@@ -9,7 +9,7 @@ public class enable : MonoBehaviour
     public GameObject enemy3;
     public GameObject enemy4;
     public GameObject enemy5;
-    public GameObject enemy6;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -19,7 +19,7 @@ public class enable : MonoBehaviour
         enemy3.SetActive(false);
         enemy4.SetActive(false);
         enemy5.SetActive(false);
-        enemy6.SetActive(false);
+        
         
     }
 
@@ -33,7 +33,6 @@ public class enable : MonoBehaviour
             enemy3.SetActive(true);
             enemy4.SetActive(true);
             enemy5.SetActive(true);
-            enemy6.SetActive(true);
             
         }
     }
